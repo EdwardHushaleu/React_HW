48d5d843d1f9052eb387f2dcc08362a1b8e99a6f
Edward Hushlew <2779522@gmail.com>
2021-11-15 13:41:25 +0300

    clicker but no Export, Import

---
+++ my-app/src/button.js
@@ -0,0 +1,18 @@
+
+
+class Button extends React.Component {
+    render() {
+      return (
+        // props с верху (потому  что это свойство)?
+        <div><button onClick={this.props.handler}>
+          Click
+        </button>
+          <div>
+            Clicks how mach {this.props.counter}
+          </div>
+        </div>
+      )
+    }
+  }
+
+export default Button
---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Content from './Result';
+
+
+
+
+// class Text extends React.Component{
+//   render(){
+//     return(
+   
+//     )
+//   }
+// }
+
+ReactDOM.render(
+  <Content />,
+  document.getElementById('root')
+);
+
