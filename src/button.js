bbc6ff1e811595151746e0bd3dbb0c7d0a5461ce
Edward_Hushalew <2779522@gmail.com>
2021-11-15 19:10:58 +0300

    test

---
+++ src/button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render() {
+        return (
+            // props с верху (потому  что это свойство)?
+            <div>
+                <button onClick={this.props.handler}>
+                    Click
+                </button>
+                <div>
+                    Clicks how mach {this.props.counter}
+                </div>
+            </div>
+        )
+    }
+}
+
+// export class Text extends React.Component {
+//     render() {
+//         return (
+
+//         )
+//     }
+// }
+
+export default Button
+
--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom';
 import Content from './Result';
 
 
-
-
-// class Text extends React.Component{
-//   render(){
-//     return(
-   
-//     )
-//   }
-// }
-
 ReactDOM.render(
   <Content />,
   document.getElementById('root')
